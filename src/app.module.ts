e89bfab29fe13c118c2556dc2fcb4cd587d295da
Aquiles Moya <u634226@T0003547940.local>
2024-11-28 08:33:43 -0300

    feat(bff.credits): Se modifico para utilizar la arquitectura Modular DDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FaqsModule } from './modules/faqs';
+import { CreditsModule } from './modules/credits/credits.module';
+import { LendingModule } from './modules/lending/lending.module';
+import { ModulesApiLoansClientModule } from './integrations/api-loans-client/src';
+import { environment } from './config';
+import { ModulesApiExtrapayClient } from './integrations/api-extrapay-client/src';
+import { LoanCheckoutServiceModule } from './integrations/loan-checkout-service/src';
+import { LoansProductsServiceModule } from './integrations/loans-products-service/src';
 
 @Module({
   imports: [
-    FaqsModule
+    FaqsModule,
+    CreditsModule,
+    LendingModule,
+    ModulesApiLoansClientModule.register({
+      urlBase: environment.urlApiLoans,
+    }),
+    ModulesApiExtrapayClient.register({
+      urlBase: environment.urlApiExtraPay,
+    }),
+    LoanCheckoutServiceModule.register({
+      urlBase: environment.urlLoanCheckoutService,
+    }),
+    LoansProductsServiceModule.register({
+      urlBase: environment.urlLoanProductService,
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { RestApplicationFactory } from '@pepa/platform-rest';
 import { AppModule } from './app.module';
+import { environment } from './config';
+import { options } from './app.options';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = environment.port;
+
+  // Rest application factory
+  const app = await RestApplicationFactory.create(AppModule, options);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port);
+  });
 }
+
 bootstrap();
